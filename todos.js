9cf7ff8d903840a152f177ae7f1ef73e5a4735d3
Blake Julander <blakejulander@gmail.com>
2023-11-01 07:06:23 -0700

    created basic functionality to add and delete values on a list

--- todos.js
+++ todos.js
@@ -1,5 +1,28 @@
-let input = prompt("what would you like todo?")
+let input = prompt('what would you like todo?')
 
-while(input !== 'quit') {
+const todos = ['Do homework', 'Study']
+
+while(input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log ('*************')
+        for (let i = 0; i < todos.length; i++){
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log ('*************')
+    } else if (input === 'new'){
+        const newTodo = prompt('Okay, what is the new todo?')
+        todos.push(newTodo)
+        console.log(`${newTodo} has been added to the list`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Okay, enter an index to delete '))
+        if(!Number.isNaN(index)){
+            const deleted = todos.splice(index, 1)
+            console.log(`Deleted ${deleted[0]}`)
+        } else {
+            console.log('Unknown index')
+        }
+    }
     input = prompt('what would you like todo?')
 }
+
+console.log('You have quit')
